6a6632e309775bac0b784c9d6e178804ec53729a
Roy Park <roypark2638@gmail.com>
2021-09-27 18:52:54 -0700

    Reverse Prefix Of Word

---
+++ ReversePrefixOfWord.py
@@ -0,0 +1,48 @@
+'''
+2000. Reverse Prefix of Word
+
+Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+Return the resulting string.
+
+Example 1:
+
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3. 
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+Example 2:
+
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+Example 3:
+
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word.
+You should not do any reverse operation, the resulting string is "abcd".
+'''
+word = "abcdefd"
+ch = "d"
+# 1. pointer
+# - iterate word and find the index of ch. enmueration
+# - reverse word by slicing and return the result
+
+
+def reversePrefix(word, ch):
+    index = -1
+    for i, c in enumerate(word):
+        if c == ch:
+            index = i
+            break
+
+    if index != -1:
+        return word[index::-1] + word[index+1:]
+    return word
+
+
+print(reversePrefix(word, ch))
