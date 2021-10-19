c60697321e0070ea1b8a5f0e3b2d01b1521c3a5b
Roy Park <roypark2638@gmail.com>
2021-10-18 20:09:59 -0700

    Binary Search

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,45 +1,50 @@
 '''
-Binary Search
+35. Search Insert Position
 
-Write a function that takes in a sorted array of integers as well as a target integer. The function should use the Binary Search algorithm to determine if the target integer is contained in the array and should return its index if it is, otherwise -1.
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
-Sample input
-array = [0,1,21,33,45,45,61,71,72,73]
-target = 33
+You must write an algorithm with O(log n) runtime complexity.
 
-Sample output
-3
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+Example 4:
+
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+Example 5:
+
+Input: nums = [1], target = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
 '''
 
-'''
-Use 3 pointers, left, right, mid.
-left = 0
-right = len(array) - 1
-mid = (left + right) //2
 
-1. in a while left <= right, check the value in the middle in the array
-  2. if the currentValue < target, then move left = mid + 1
-  3. elif the currentValue > target, then move right = mid - 1
-  4. else the currentValue == target, return the mid
-5. if we don't find the target number, return - 1
-'''
-
-array = [0,1,21,33,45,45,61,71,72,73]
-target = 33
-
-def binarySearch(array, target):
-  left = 0
-  right = len(array) - 1
-
-  while left <= right:
-    mid = (right + left) // 2 # 9 - 0 / 2 = 4
-    currentValue = array[mid]
-    if target < currentValue:
-      right = mid - 1
-    elif target > currentValue:
-      left = mid + 1
-    else:
-      return mid
-  return -1
-    
-print(binarySearch(array, target))
+class Solution(object):
+    def searchInsert(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + (r-l)//2
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m-1
+            else:
+                l = m+1
+        return l
